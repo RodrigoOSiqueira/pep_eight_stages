7087df0a54bb49b44b7756820e143a7f8896f25b
bibicobolado <rodrigosiqueira@id.uff.br>
2020-01-07 00:35:28 -0300

    changing filabase

--- fila_base.py
+++ fila_base.py
@@ -1,4 +1,7 @@
-class FilaBase:
+import abc
+
+
+class FilaBase(metaclass=abc.ABCMeta):
 
     codigo = 0
     fila = []
@@ -10,3 +13,19 @@ class FilaBase:
             self.codigo = 0
         else:
             self.codigo += 1
+
+    @abc.abstractmethod
+    def gera_senha_atual(self):
+        ...
+
+    @abc.abstractmethod
+    def chama_cliente(self, caixa):
+        ...
+
+    @abc.abstractmethod
+    def estatistica(self, dia, agencia, flag_detail):
+        ...
+
+    @abc.abstractmethod
+    def atualiza_fila(self):
+        ...
